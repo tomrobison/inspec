4a2dab1240f1f47998ab0741a4d30c658dbc14eb
Nick Schwaderer <nschwaderer@chef.io>
2020-05-01 12:14:45 +0100

    Moves namespace clash control to waivers fixture
    Signed-off-by: Nick Schwaderer <nschwaderer@chef.io>

--- test/fixtures/profiles/old-examples/profile/controls/example-namespace-clash.rb
+++
@@ -1,8 +0,0 @@
-title "Namespace clashing control"
-
-control 'my_control' do
-only_if { input('my_control', value: 'false') == 'false' }
-  describe file("/tmp") do
-    it { should be_directory }
-  end
-end
